dbc3037c74275af03ef893b0ce1879302fe46e4f
Aaron <Aaron9034@Hotmail.com>
2021-01-13 09:30:02 -0600

    new loop added
    Code can now be run over and over again via a loop

--- fake_search.py
+++ fake_search.py
@@ -21,17 +21,73 @@ import enterprise_extensions
 from enterprise_extensions import models, sampler, model_utils
 from multiprocessing import Pool
 
-# import the par and tim files
-datadir = './fakes_gwb3'
+import libstempo as libs
+import libstempo.plot as LP, libstempo.toasim as LT
 
-parfiles = sorted(glob.glob(datadir + '/par/' + '*.par'))
-timfiles = sorted(glob.glob(datadir + '/tim/' + '*.tim'))
 
-psrs = []
-for p, t in zip(parfiles, timfiles):
-    psrname = parfiles[0].split('/')[-1].split('_')[0]
-    psr = Pulsar(p, t, ephem='DE438')
-    psrs.append(psr)
+
+def make_fakes(amp, psrcut):
+    datadir = './data/partim/'
+    ## Get parameter noise dictionary
+    noisedir = os.getcwd() + '/data/noisefiles/'
+
+    noise_params = {}
+    for filepath in glob.iglob(noisedir + '/*.json'):
+        with open(filepath, 'r') as f:
+            noise_params.update(json.load(f))
+    # print(noise_params)
+
+    # import par and tim files into parfiles and timfiles
+    tempopsr = []
+    print('Importing to libstempo....')
+    for psr in psrcut:
+        parfiles = sorted(glob.glob(datadir + psr.name + '*.par'))
+        timfiles = sorted(glob.glob(datadir + psr.name + '*.tim'))
+        psr = libs.tempopulsar(parfile = parfiles[0],
+                               timfile = timfiles[0], maxobs=50000)
+        tempopsr.append(psr)
+    print('adding efac')
+    for psr in tempopsr:
+        LT.make_ideal(psr)
+        LT.add_efac(psr, efac=1.0)
+    print('adding red noise')
+    for psr in tempopsr:
+        # print(psr.name)
+        rednoise_toadd = noise_params[str(psr.name) + '_log10_A']
+        # print(psr.name)
+        gamma = noise_params[str(psr.name) + '_gamma']
+        LT.add_rednoise(psr, 10**rednoise_toadd, gamma, components=30)
+        psr.fit()
+    print('adding GWB')
+    LT.createGWB(tempopsr, Amp=amp, gam=13./3.)
+    for psr in tempopsr:
+        psr.fit()
+    # save the pulsars
+    pardir = './fakes_gwb/par/'
+    timdir = './fakes_gwb/tim/'
+    if not os.path.exists(pardir):
+        os.makedirs(pardir)
+        os.makedirs(timdir)
+
+    print('fixing tim files')
+    for psr in tempopsr:
+        psr.savepar(pardir + psr.name + '-sim.par')
+        psr.savetim(timdir + psr.name + '-sim.tim')
+        libs.purgetim(timdir + psr.name + '-sim.tim')  # fix the tim files
+
+
+def load_fakes():
+    # import the par and tim files
+    datadir = './fakes_gwb'
+
+    parfiles = sorted(glob.glob(datadir + '/par/' + '*.par'))
+    timfiles = sorted(glob.glob(datadir + '/tim/' + '*.tim'))
+
+    psrs = []
+    for p, t in zip(parfiles, timfiles):
+        psrname = parfiles[0].split('/')[-1].split('_')[0]
+        psr = Pulsar(p, t, ephem='DE438')
+        psrs.append(psr)
 
 
 def gwb_ul(psrs_cut):
@@ -95,10 +151,7 @@ def gwb_ul(psrs_cut):
     return UL
 
 
-# In[20]:
-
-
-def find_ul(psrs, best_list, ul_list, i):
+def find_ul(psrs, best_list, ul_list, k, i):
     # for i in range(start, len(psrs)):  # find the best pulsar each time
     psrscut = []  # pulsars to be tested
     for num in best_list:
@@ -108,33 +161,52 @@ def find_ul(psrs, best_list, ul_list, i):
     print(psrs[i].name)
     if i in best_list:  # don't double up on pulsars
         print('psr in list already... skipping....')
-        ul = 0
+        ul = 1
     else:
         print('number of pulsars =', len(psrscut))
         ul = gwb_ul(psrscut)[0]
-    fname = 'ul_data_{0}_psrs_pid_{1}.txt'.format(len(best_list) + 1, os.getpid())
+    fname = 'ul_data_{0}_run_{1}.txt'.format(len(best_list) + 1, k)
     with open(fname, 'ab') as f:
-        np.savetxt(f, [i, ul])  # save the upper limits to file
+        np.savetxt(f, np.c_[i, ul])  # save the upper limits to file
     # best_list.append(np.argmin(ul_list)[0]) # append the best ul to best_list
     return ul
 
-def multi_ul(i):
-    ul = find_ul(psrs, best_list, ul_list, i)
+
+def multi_ul(k, i):
+    ul = find_ul(psrs, best_list, ul_list, k, i)
     return ul
 
-best_list = []  # to store pulsar order
-filename = 'best_list.txt'
-for j in range(len(best_list), len(psrs)):  # cycle through once for each pulsar
-    ul_list = []
-    pool = Pool(processes=22)
-    upper = pool.map(multi_ul, range(len(psrs)))
-    ul_list.append(upper)
-    pool.close()
-    pool.join()
-    best_ul = np.argmin(ul_list)[0]
-    best_list.append(best_ul)
-    with open(filename, 'ab') as f:
-        np.savetxt(f, np.c_[j, best_ul])
+
+# load real data
+with open("psrs.p", "rb") as f:
+    psrs = pickle.load(f)
+
+
+num_trials = 10  # number of realizations
+amp = 3e-15
+n = 0  # number of times the trials go lower than GWB injection
+for k in range(num_trials):
+
+    make_fakes(amp, psrs)  # make fake pulsar set
+    best_list = []  # to store pulsar order
+    filename = 'best_list_run_{0}_amp_{1}.txt'.format(k, amp)
+    for j in range(len(best_list), len(psrs)):  # cycle through once for each pulsar
+        ul_list = []
+        for i in range(len(psrs)):
+            upper = multi_ul(k, i)
+            ul_list.append(upper)
+        best_ul = np.argmin(ul_list)
+        best_list.append(best_ul)
+        with open(filename, 'ab') as f:
+            np.savetxt(f, np.c_[k, j, best_ul])
+        if best_ul < amp:
+            n += 1
+            break
+        elif j == 5 and best_ul > amp:
+            print('With 5 pulsars, the ul is still above the GWB injection.')
+            break
+
+
 
 
 
